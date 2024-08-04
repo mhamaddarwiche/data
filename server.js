e1a857410bc204ab355f33d5978ae0a4d5ca94ff
mhamad <darwiche.mhamad3@gmail.com>
2024-08-04 10:16:01 +0200

    c1

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors');
+const xlsx = require("xlsx");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+const wb = xlsx.readFile('inventory.xlsx');
+const ws = wb.Sheets["data"];
+
+
+app.use(cors())
+
+//app.get('/', function (req, res) {
+    //res.send('Hello World')
+//})
+
+app.get('/data', function (req, res) {
+    let data = xlsx.utils.sheet_to_json(ws);
+    res.send(data);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`localhost: ${PORT}`);
+})
